62b7003b025a6d068b7c79ea6717c710e918cf43
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-09-08 13:47:41 -0300

    feat: :sparkles: manipulate amount field and add formatCurrencyBRL

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-console.log("javascript connected!");
+// Seleciona os elementos do formulário.
+const amount = document.getElementById("amount");
+
+// Captura o evento de input para formatar o valor.
+amount.oninput = () => {
+  // Obtém o valor atual do input e remove os caracteres não numéricos.
+  let valueWithOnlyNumbers = amount.value.replace(/\D/g, "");
+
+  // Transforma o valor em centavos (exemplo: 150/100 = 1.5 que é equivalente a R$ 1,50).
+  valueWithOnlyNumbers = Number(valueWithOnlyNumbers) / 100;
+
+  amount.value = formatCurrencyBRL(valueWithOnlyNumbers);
+};
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL (Real Brasileiro).
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  // Retorna o valor formatado.
+  return value;
+}
